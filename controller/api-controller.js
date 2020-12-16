9dd2404824eb3452a86783215f5e042b32c1d217
Bobur-kobilov <bobur0114jon@gmail.com>
2020-12-16 15:00:03 +0900

    api added

--- controller/api-controller.js
+++ controller/api-controller.js
@@ -16,9 +16,9 @@ async function getBalance(address) {
     return e;
   };
 };
-async function getTransHist(address) {
+async function getTransHist(address,index,limit) {
   try {
-    const response = await axios.get(`${config.dev.base_url + '/address/' + address + '/basic/transactions'}`,{
+    const response = await axios.get(`${config.dev.base_url + '/address/' + address + '/basic/transactions?' + 'index=' + index + '&' + 'limit='+ limit}`,{
         headers: {
           "Content-Type": "application/json",
           "X-API-Key": config.dev.api_key
@@ -43,9 +43,22 @@ async function getUxto(address) {
     console.error(e);
     return e;
   }
+};
+async function sendSignedTx(hex) {
+  try {
+    const payload = {
+      hex: hex
+    };
+    const response = await axios.post(`${config.dev.base_url} + '/txs/send/'`,payload);
+    return response;
+  } catch (e) {
+    console.error(e);
+    return e;
+  }
 }
 module.exports = {
   getBalance,
   getTransHist,
-  getUxto
+  getUxto,
+  sendSignedTx
 } 
--- routes/api.js
+++ routes/api.js
@@ -4,19 +4,22 @@ const router = express.Router();
 const config = require('../config/config.json');
 const apiController = require('../controller/api-controller');
 
-const addr = '2N6HeA8vi3LieVEpqz5ZBdcYzXpzTR55sT4';
 router.get('/balance', async function(req,res) {
   try {
-    const response = await apiController.getBalance(addr);
+    const address = req.query.address;
+    const response = await apiController.getBalance(address);
     res.send(response.data);
   } catch (e) {
     console.error(e);
     return e;
   };
 });
-router.post('/deposit', function(req,res) {
+router.post('/tx/send', function(req,res) {
   try {
-    res.send('Deposit')
+    const address = req.body.hex;
+    const hex = '02000000012ef6ff4aaa76aaff4bea235df134923a830a89d2fbdea5fdc330c9a42eb920a8010000006a47304402205c44fb58b3eaa907cccb2cac87749f00cb52f0d050d183ebba80d672413b9a540220749c8b53665db9f36d5e760ad627b0e22072a6cf91a5d77d35ac2b95d4c1ea54012102275753690ab58df3c923001e94d407e30b03e60b1f2461729a1dd4f37ebe2469ffffffff02c8320000000000001976a914481e003d23566c1789dc9362085c3a0876570c7c88ac80380100000000001976a9147b9a627a184897f10d31d73d87c2eea191d8f50188ac00000000';
+    const response = apiController.sendSignedTx(hex);
+    res.send(response.data);
   } catch (e) {
     console.error(e);
     return e;
@@ -24,7 +27,10 @@ router.post('/deposit', function(req,res) {
 });
 router.get('/transaction', async function(req,res) {
   try {
-    const response = await apiController.getTransHist(addr);
+    const address = req.query.address;
+    const index = req.query.index;
+    const limit = req.query.limit;
+    const response = await apiController.getTransHist(address,index,limit);
     res.send(response.data);
   } catch(e) {
     console.error(e);
@@ -47,7 +53,8 @@ router.get('/account', async function(req, res) {
 });
 router.get('/uxto', async function(req, res) {
   try {
-    const response = await apiController.getUxto(addr);
+    const address = req.query.address;
+    const response = await apiController.getUxto(address);
     res.send(response.data);
   } catch (e) {
     console.error(e);
